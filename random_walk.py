1a2d07eaacdc129b2df2d660e49a9adea8e8c4b1
Roshan Aryal <yuvrajaryal83@gmail.com>
2023-02-23 22:22:43 +0545

    animate random walk

--- random_walk.py
+++ random_walk.py
@@ -1,32 +1,47 @@
-""" THIS IS A BUGGY ONE
-    It will take some time to complete this project.
- """
- # 1D implementation 
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+from matplotlib.animation import FuncAnimation
 
 origin = 0
 step = origin
-coin_states = ['H', 'D']
+step_lenght = 1
+coin_states = ['H', 'T']
 
-random_steps = []
-time_stamp = []
-count = 0
+x_steps = []
+y_steps = []
 
-while count < 1000:
+fig, ax = plt.subplots(figsize=(6, 5))
+ax.set(xlim=(-10, 10), ylim=(-50, 50))
 
+while True:
+    if step == 10:
+        break
     if random.choice(coin_states) == 'H':
         step += 1
-        count += 1
-        random_steps.append(step)
-        time_stamp.append(count)
+        x_steps.append(step)
     else:
         step -= 1
-        count += 1
-        random_steps.append(step)
-        time_stamp.append(count)
+        x_steps.append(step)
 
+y_steps = np.cumsum(x_steps)
 
-plt.plot(time_stamp, random_steps)
+line = ax.plot(y_steps[0], x_steps[0], color='k', lw=2)[0]  # plotting the first line
+
+# aimation function to change the line every frame
+def animate(i):
+    line.set_data(y_steps[:i], x_steps[:i])
+    return line
+
+print(len(x_steps))
+# calling the funcanimation function
+anim = FuncAnimation(fig, animate, interval=100, cache_frame_data=False)
+
+plt.draw()
 plt.show()
+
+
+
+
+
+
